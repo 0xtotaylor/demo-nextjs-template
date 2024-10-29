d0f03e2d7e9f67dee8f9da4eec5615f8516f1b06
Tomas Taylor <totaylor19@gmail.com>
2024-10-29 15:37:16 -0400

    Update schema.ts

--- src/constants/schema.ts
+++ src/constants/schema.ts
@@ -5,13 +5,13 @@ export const verticSchema = {
       url: "{baseUrl}",
       variables: {
         baseUrl: {
-          default: "https://api-qa.skyfire.xyz/v1/receivers/vetric",
+          default: "https://api-qa.skyfire.xyz",
         },
       },
     },
   ],
   paths: {
-    "/twitter/top": {
+    "/v1/receivers/vetric/twitter/top": {
       get: {
         deprecated: false,
         parameters: [
@@ -30,7 +30,7 @@ export const verticSchema = {
         operationId: "topTweets",
       },
     },
-    "/linkedin/people-search": {
+    "/v1/receivers/vetric/linkedin/people-search": {
       get: {
         deprecated: false,
         parameters: [
@@ -47,6 +47,25 @@ export const verticSchema = {
         description:
           "This endpoint retrieves the top results for a given query, making it ideal for finding the most popular tweets related to a specific keyword or topic.",
         operationId: "topTweets",
+      },
+    },
+    "/proxy/vetric-facebook/facebook/v1/search/users": {
+      post: {
+        deprecated: false,
+        parameters: [
+          {
+            name: "typed_query",
+            in: "query",
+            required: true,
+            schema: {
+              type: "string",
+            },
+            description: "The text query for searching users",
+          },
+        ],
+        description:
+          "This endpoint searches for Facebook users based on the provided text query.",
+        operationId: "searchFacebookUsers",
       },
     },
   },
