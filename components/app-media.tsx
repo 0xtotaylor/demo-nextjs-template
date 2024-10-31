79518510c877ce397c7725535c34a7d5e3997654
Tomas Taylor <totaylor19@gmail.com>
2024-10-31 11:58:27 -0400

    Update app-media.tsx

--- components/app-media.tsx
+++ components/app-media.tsx
@@ -4,12 +4,22 @@ export function AppMedia() {
   const { urls } = useMedia()
 
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4">
-      <div className="grid auto-rows-min gap-4 md:grid-cols-5">
-        {Array.from({ length: 20 }).map((_, i) => (
-          <div key={i} className="aspect-square rounded-xl bg-muted/50" />
+    <>
+      <div className="flex flex-1 flex-col gap-4 p-4">
+        <div className="grid auto-rows-min gap-4 md:grid-cols-5">
+          {Array.from({ length: 10 }).map((_, i) => (
+            <div key={i} className="aspect-square rounded-xl bg-muted/50" />
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-1 flex-col gap-4 p-4">
+        {Array.from({ length: 6 }).map((_, index) => (
+          <div
+            key={index}
+            className="aspect-video h-12 w-full rounded-lg bg-muted/50"
+          />
         ))}
       </div>
-    </div>
+    </>
   )
 }
